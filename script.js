2a691aa2f0669515c6bcd3671e4ab66532118953
hamza1144 <54630149+hamza1144@users.noreply.github.com>
2020-11-30 13:03:42 +0500

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+function update(){
+    var input = document.getElementById("myinput");
+    console.log(input.value);
+    var output = document.getElementById("myoutput");
+    output.innerHTML = input.value;
+}
